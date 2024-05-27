ccbde31800589365dec73f7a5ca5d132d0c24f4b
Fernanda Tomita <fernandabio45@gmail.com>
2024-05-27 18:29:53 -0300

    criação da classe Conversor com seus métodos declarados, porém sem a lógica das operações, apenas retornando um valor padrão, para que os testes sejam criados antes

---
+++ README.md
@@ -0,0 +1,15 @@
+#Conversor de Temperatura
+
+Projeto criado para fins de estudo de TDD (Test Driven Development)
+
+Regras:
+- operações realizadas: 
+    - converter Celsius e Fahrenheit
+    - converter Celsius e Kelvin
+    - converter Kelvin e Celsius
+    - converter Kelvin e Fahrenheit
+    - converter Fahrenheit e Kelvin
+    - converter Fahrenheit e Celsius
+- 1 entrada para cada operação
+- tipo decimal para valores de temperatura
+- retornar histórico das últimas 3 operações realizadas
--- conversor-tdd/Conversor.cs
+++ conversor-tdd/Conversor.cs
@@ -7,6 +7,37 @@ namespace conversor_tdd
 {
     public class Conversor
     {
+        public decimal ConverterCelsiusParaFahrenheit(decimal temperatura)
+        {
+            return 0;
+        }
+        public decimal ConverterCelsiusParaKelvin(decimal temperatura)
+        {
+            return 0;
+        }
         
+        public decimal ConverterKelvinParaCelsius(decimal temperatura)
+        {
+            return 0;
+        }
+        public decimal ConverterKelvinParaFahrenheit(decimal temperatura)
+        {
+            return 0;
+        }
+        public decimal ConverterFahrenheitParaKelvin(decimal temperatura)
+        {
+            return 0;
+        }
+
+        public decimal ConverterFahrenheitParaCelsius(decimal t1, decimal t2)
+        {
+            return 0;
+        }
+
+        public List<string> historico()
+        {
+            return new List<string>();
+        }
+
     }
 }
